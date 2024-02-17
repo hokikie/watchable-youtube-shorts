86b7c2d813b91647f2ea3fade768cae81d66d63a
hohokie <nhpunch@gmail.com>
2024-02-17 16:23:15 +0900

    feat: add view options functionality to popup.js - update watchOptions - sync chrome extension storage

--- popup.js
+++ popup.js
@@ -1,1 +1,26 @@
-// git keep
+let watchOptions = {
+  isHide: false,
+  isAside: false,
+};
+
+function updateWatchOptions(key, value) {
+  watchOptions[key] = value;
+  chrome.storage.sync.set({ watchOptions });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hideOverlay = document.getElementById('hideOverlay');
+  const asideOverlay = document.getElementById('asideOverlay');
+
+  chrome.storage.sync.get('watchOptions', ({ watchOptions = {} }) => {
+    hideOverlay.checked = watchOptions.isHide || false;
+    asideOverlay.checked = watchOptions.isAside || false;
+  });
+
+  hideOverlay.addEventListener('change', () => {
+    updateWatchOptions('isHide', hideOverlay.checked);
+  });
+  asideOverlay.addEventListener('change', () => {
+    updateWatchOptions('isAside', asideOverlay.checked);
+  });
+});
